0c00632b6deaccc9224ab5623eb23dd3353c4e8d
Prabesh Bhandari <prabeshbhandari842@gmail.com>
2025-12-02 13:51:46 +0545

    inforce same on all remaining pages

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { prisma } from './prisma'
